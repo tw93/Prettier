cba601e9a1385ab141a93e634cebb2865bf0e364
Tw93 <tw93@qq.com>
2022-08-28 11:37:10 +0800

    :bug: 暂时兼容一下

--- Sources/Prettier/Bundle+.swift
+++ Sources/Prettier/Bundle+.swift
@@ -1,5 +1,5 @@
 #if !SWIFT_PACKAGE
 extension Bundle {
-  static var module:Bundle { Bundle(identifier: "com.library.example")! }
+  static var module:Bundle { Bundle(identifier: "com.tw93.miaoyan")! }
 }
 #endif
